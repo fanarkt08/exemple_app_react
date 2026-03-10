396b808900271f61b6c01214162797daba5e88ed
fanarkt08 <fanarkt08@gmail.com>
2026-03-10 11:51:30 +0100

    update activity 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,10 @@
         </title>
     </head>
     <body>
-        <header>
-            <h1>Mon site Laravel</h1>
-        </header>
+        @include('partials.header')
         <main>
             @yield('content')
         </main>
-        <footer>
-            <p>© 2025 - Tous droits réservés</p>
-        </footer>
+        @include('partials.footer')
     </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <h1>Mon site Laravel</h1>
+    <nav>
+        <a href="/">Accueil</a>
+    </nav>
+</header>
