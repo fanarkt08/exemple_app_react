6a7125fcedef6147f61979d024283b86237e2c4b
fanarkt08 <fanarkt08@gmail.com>
2026-03-10 16:03:59 +0100

    update activity 14 namespace

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
         <x-article 
             :title="$article['title']"
-            :description="\Illuminate\Support\Str::limit($article['description'], 30)"
+            :description="Str::limit($article['description'], 30)"
         />
 
     @endforeach
