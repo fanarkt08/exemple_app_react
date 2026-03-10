102e447fd34ecc1cad34fbf9f23132ef46734dbb
fanarkt08 <fanarkt08@gmail.com>
2026-03-10 14:00:01 +0100

    update activity 11

---
+++ app/View/Components/Article.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Article extends Component
+{
+    public $title;
+    public $description;
+
+    public function __construct($title, $description)
+    {
+        $this->title = $title;
+        $this->description = $description;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.article');
+    }
+}
---
+++ resources/views/components/article.blade.php
@@ -0,0 +1,5 @@
+<!-- resources/views/components/article.blade.php -->
+<article class="article">
+    <h2>{{ $title }}</h2>
+    <p>{{ $description }}</p>
+</article>
