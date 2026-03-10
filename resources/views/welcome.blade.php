663c6bea93715609e1f217a8634be2750c868c1f
fanarkt08 <fanarkt08@gmail.com>
2026-03-10 15:02:17 +0100

    update activity 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,15 @@
 @section('content')
     <h2>Bienvenue sur le site de {{ $name }}</h2>
     @foreach($articles as $article)
-    <x-article 
-        :title="$article['title']"
-        :description="$article['description']"
-    />
-@endforeach
+
+        @if($loop->last)
+            @break
+        @endif
+
+        <x-article 
+            :title="$article['title']"
+            :description="$article['description']"
+        />
+
+    @endforeach
 @endsection
